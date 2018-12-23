642659b3fc7d5eb4ea310ae6b23e9cc51b750110
Lalu John <laluvjohn@gmail.com>
2018-12-23 11:10:16 -0600

    About to start cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,7 +27,7 @@ const Layout = ({ children }) => (
             paddingTop: 0,
           }}
         >
-          {children}
+        { children }
         </div>
       </>
     )}
---
+++ src/pages/page3.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Page3 = () => 
+  (
+    <div>
+      WOO PAGE 3!
+    </div>
+  )
+
+export default Page3
